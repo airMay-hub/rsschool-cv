5e2d036b3db76edaf7065fb17c5f43b953c0c1e1
airMay-hub <kiiikaaa.93@mail.ru>
2021-09-19 19:20:41 +0300

    fix: add close menu when click menu item

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 // Меню бургер
 
-  const menuBurger = document.querySelector('.navigation__burger');
-  if (menuBurger) {
-    const navigation = document.querySelector('.navigation__body');
-    menuBurger.addEventListener("click", function (e) {
-      document.body.classList.toggle('lock');
-      menuBurger.classList.toggle('navigation__burger_active');
-      navigation.classList.toggle('navigation__body_active');
-    });
-  }
+const menuBurger = document.querySelector('.navigation__burger');
+const navigation = document.querySelector('.navigation__body');
+const navigationItems = document.querySelectorAll('.navigation__link');
+
+if (menuBurger) {
+  menuBurger.addEventListener("click", function (e) {
+    document.body.classList.toggle('lock');
+    menuBurger.classList.toggle('navigation__burger_active');
+    navigation.classList.toggle('navigation__body_active');
+  });
+}
+
+function removeActive(e) {
+  navigationItems.forEach(item => {
+    if(e.target === item && navigation.classList.contains('navigation__body_active')) {
+      document.body.classList.remove('lock');
+        menuBurger.classList.remove('navigation__burger_active');
+        navigation.classList.remove('navigation__body_active');
+    }
+  })
+}
+
+navigation.addEventListener('click', removeActive);
